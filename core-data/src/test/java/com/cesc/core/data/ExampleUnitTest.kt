183d94861a036da3df7754d6ec891de1247c4b7f
cesclong <cesclong@163.com>
2022-06-06 22:53:22 +0800

    add unit test dependcy framework

--- core-data/src/test/java/com/cesc/core/data/ExampleUnitTest.kt
+++ core-data/src/test/java/com/cesc/core/data/ExampleUnitTest.kt
@@ -1,17 +1,42 @@
 package com.cesc.core.data
 
+import com.cesc.core.data.repository.HomeBannerRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.koin.ksp.generated.module
+import org.koin.test.KoinTest
+import org.koin.test.KoinTestRule
+import org.koin.test.get
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+@OptIn(ExperimentalCoroutinesApi::class)
+class ExampleUnitTest : KoinTest {
+    @get:Rule
+    val koinTestRule = KoinTestRule.create {
+        modules(
+            DataDIModule().module
+        )
+    }
+
+    @Test
+    fun `fetch banner data`() = runTest {
+        val bannerRepository = get<HomeBannerRepository>()
+        val result = bannerRepository()
+        println(result)
+        assertEquals(false, result.isEmpty())
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
 }
